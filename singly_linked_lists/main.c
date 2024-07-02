0056266aa33e2f6f42955190ebac840c0478e04e
3lackrukh <nathan.rhys@atlasschool.com>
2024-07-02 12:16:43 +0000

    created files for live code

---
+++ singly_linked_lists/main.c
@@ -0,0 +1,30 @@
+int main()
+{
+    node_t *head = NULL;
+    
+    // Adding nodes one at a time
+    add_node(&head, "Try");
+    add_node(&head, "Force");
+    
+    printf("List: ");
+    // Correctly initializing hookshot here
+    node_t *hookshot = head;
+    while (hookshot!= NULL)
+    {
+        printf("%s -> ", hookshot->data);
+        hookshot = hookshot->next;
+    }
+    printf("End\n");
+
+    // Freeing memory
+    while (head!= NULL) 
+    {
+      //REMEMBER: set the node POINTER hookshot to head
+        node_t *hookshot = head;
+        head = head->next;
+        free(hookshot->data);
+        free(hookshot);
+    }
+
+    return 0;
+}
